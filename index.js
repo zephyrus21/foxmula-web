4ba36920eb0788897f63c24fa19035779700a9bf
zephyrus21 <pandey.piyus@gmail.com>
2021-02-25 21:33:28 +0530

    adding main contents

---
+++ index.js
@@ -0,0 +1,29 @@
+const data = fetch('./data.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((res) => {
+    getFeatured(res);
+    getAll(res);
+  });
+
+const getFeatured = (result) => {
+  const main = document.querySelector('.main-contents');
+  const data = result.data;
+
+  data.map((res) => {
+    if (res.display === 'featured') {
+      var div = document.createElement('div');
+      div.classList.add('featured');
+      div.innerHTML = `
+      <p class="batch">Batch Name: ${res.id}</p>
+      <p class="sub">${res.sub}, ${res.class}th ${res.board}</p>
+      <p class="str">${res.strength} Students</p>
+      <p class="batch">${res.days
+        .map((day) => `<p>DAY: ${day}</p>`)
+        .join('')}</p>
+      `;
+      main.appendChild(div);
+    }
+  });
+};
